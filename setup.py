62368add5a337e6d88e8568a81c6b14fdf9f0f03
Lenar Imamutdinov <imamutdinov@lexion-development.ru>
2016-07-22 15:25:28 +0300

    fixed readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Lagring",
-    version="0.2.5",
+    version="0.2.6",
     author="Lenar Imamutdinov",
     author_email="lenar.imamutdinov@gmail.com",
     packages=find_packages(),
